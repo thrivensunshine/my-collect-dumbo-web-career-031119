8e7bb6b624537e0431ec207527f69718fded8a2c
DANIELLE ALBA <danib.sgh@gmail.com>
2019-02-23 22:57:04 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,11 @@
 def my_collect(arr)
-  if block_given?
-    i= 0
-    
-    while arr.length > i 
-    i.upcase
+if block_given?
+    i = 0
+    answer = []
+    while i < arr.length
+        answer << yield(arr[i])
+        i += 1
+    end
+    answer
 end
 end
-end
-
-
