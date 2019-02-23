068826b16328eae21a36581a5a90a0e7cabcf201
DANIELLE ALBA <danib.sgh@gmail.com>
2019-02-23 22:59:56 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(arr)
+if block_given?
+    i = 0
+    ans = []
+    while i < arr.length
+        ans << yield(arr[i])
+        i += 1
+    end
+    ans
+end
+end
 
-
+# needs more review 
